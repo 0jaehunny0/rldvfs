430e4dc1bc1f1296c59e2f7b44f7c919475c2208
zambbo <rhkrqnwk1@naver.com>
2024-07-29 22:14:43 +0900

    add basic app process packet monitoring script
    Signed-off-by: zambbo <rhkrqnwk1@naver.com>

---
+++ test_packet.py
@@ -0,0 +1,46 @@
+import subprocess
+import sys
+import time
+
+def get_fps(_window):
+    msg = 'adb shell dumpsys SurfaceFlinger --latency ' + '"' + _window + '"'
+    result = subprocess.run(msg.split(), stdout=subprocess.PIPE)
+    result = result.stdout.decode('utf-8')
+    result = result.split("\n")
+
+    startTime = int(result[-23].split("\t")[0])
+    lastTime = int(result[-3].split("\t")[-1])
+    twentyFrameTime = (lastTime - startTime) / 1000000000
+    fps = 20 / twentyFrameTime
+    return fps
+
+def get_packet_info(proc_num: int) -> tuple[int, int]:
+    msg = f"adb shell cat /proc/{proc_num}/net/dev"
+    result = subprocess.run(msg.split(), stdout=subprocess.PIPE)
+    result = result.stdout.decode("utf-8")
+    result = result.split('\n')
+    result = list(filter(lambda l: 'wlan0' in l, result))[0]
+    result = result.split()
+    received_packet, transmitted_packet = int(result[2]), int(result[10])
+
+    return received_packet, transmitted_packet
+
+def monitor_packets(proc_num:str, interval:int = 0.5):
+    prev_received, prev_transmitted = get_packet_info(proc_num)
+    
+    while True:
+        time.sleep(interval)
+        cur_received, cur_transmitted = get_packet_info(proc_num)
+
+        received_diff, transmitted_diff = cur_received - prev_received, cur_transmitted - prev_transmitted
+        print(f"Received: {received_diff}, transmitted: {transmitted_diff}")
+
+        prev_received, prev_transmitted = cur_received, cur_transmitted
+
+def main():
+    proc_num = sys.argv[1]
+    print(f"Process num: {proc_num}")
+    monitor_packets(proc_num)
+
+if __name__ == '__main__':
+    main()
